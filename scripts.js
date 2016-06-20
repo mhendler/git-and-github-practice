12213c679ab40cb530d44ad3d4a4d6d8e8a7fe04
mhendler <mjhendler@yahoo.com>
2016-06-20 11:44:38 -0600

    mailing list changes

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
-//JavaScript to be added
+window.onload  = function() {
+	getUserEmail();
+	};
+
+function getUserEmail() {
+	var buttonSelect = document.querySelector("#button-yellow");
+	buttonSelect.addEventListener('click', email);
+}
+
+function email() {
+	var email = prompt('Please enter your email'); 
+
+	if(email !== null) {
+		console.log(email);
+	}
+}
+
+
